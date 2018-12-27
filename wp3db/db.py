99dde94d7ee13402ce2ea138001d9be9519c872f
trk <dev.trk.9001@gmail.com>
2018-12-27 14:23:45 +0600

    Reformat database code

--- wp3db/db.py
+++ wp3db/db.py
@@ -1,11 +1,14 @@
-import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from .models import Base
 
-engine = sqlalchemy.create_engine('sqlite:///wp3users.db')
+# Specify database connection
+engine = create_engine('sqlite:///wp3users.db')
 
+# Create all the tables from models that subclassed Base
 Base.metadata.create_all(engine)
-Base.metadata.bind = engine
 
+# Create a Session class for external interfacing (see __init__.py)
+Base.metadata.bind = engine
 Session = sessionmaker(bind=engine)
